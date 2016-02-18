06a98efa6b9372ea5b8c50c49067896be7ca3272
kevinetta <kevin.etta@marquette.edu>
2016-02-18 15:06:10 -0600

    hi

--- ExtremelyDifficultQuiz/src/Stuff.java
+++ ExtremelyDifficultQuiz/src/Stuff.java
@@ -14,6 +14,11 @@ public class Stuff {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// our first order of business
+		int x = 1;
+		int y = 2;
+		int z = x+y;
+		System.out.println("z is " + z);
 
 	}
 
