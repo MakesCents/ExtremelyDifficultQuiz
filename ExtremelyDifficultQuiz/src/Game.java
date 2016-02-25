858c723d92fde75417491682dd1967c21f3575ba
tbuente15 <tbuente@yahoo.com>
2016-02-25 15:16:08 -0600

    added an Enigne that runs at crazy huge fps

--- ExtremelyDifficultQuiz/src/Game.java
+++ ExtremelyDifficultQuiz/src/Game.java
@@ -11,24 +11,62 @@ public class Game {
 
 	public Window window;
 	public static int WIDTH = 1200, HEIGHT = 900;
+	private boolean running = false;
 	
 	public Game(){
 //		BackgroundMusic music = new BackgroundMusic();
 //		music.music();
 		window = new Window(WIDTH, HEIGHT, "Extremely Difficult Quiz");
+		start();
 	}
 	
 	public void start(){
-		
+		running = true;
+		run();
 	}
 	
 	public void stop(){
-		
+		running = false;
 	}
 	
 	public void run(){
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while (running){
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1){
+				tick();
+				delta--;
+			}
+			if (running){
+				render();
+			}
+			frames++;
+			
+			//Display the FPS in the console
+			if (System.currentTimeMillis() - timer > 1000){
+				timer += 1000;
+				System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+	
+	public void tick(){
 		
 	}
+	
+	public void render(){
+		
+	}
+	
 	public static void main(String[] args){
 		Game game = new Game();
 	}
