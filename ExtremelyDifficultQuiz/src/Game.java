8a8c81a68e0aa127d08eb19b4aec0d36f042485c
MakesCents <jaredfunk28@gmail.com>
2016-03-01 22:55:18 -0600

    Be more careful about uploading things; if you don't have matching folders and levels and stuff it'll create repeats. Also watch out for file references in case something got screwy.

--- ExtremelyDifficultQuiz/src/Game.java
+++ ExtremelyDifficultQuiz/src/Game.java
@@ -1,74 +1,171 @@
 /*
- * Author: Timothy Buente
- * Date: 2/18/2016
+ * Author: Matthew Kinzler
+ * Date: 2/26/2016
  * 
- * Gonna try and make a window pop up for the game.
+ * Adding graphics
  */
 
 package src;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
 	public Window window;
-	public static int WIDTH = 1200, HEIGHT = 900;
+	public static final int WIDTH = 1200, HEIGHT = 900;
+	public static final String TITLE = "Extremely Difficult Quiz";
+
 	private boolean running = false;
-	
+	private Thread thread;
+	private Menu menu;
+	private Handler handler;
+	private Q1 q1;
+	private Correct correct;
+
 	public Game(){
-//		BackgroundMusic music = new BackgroundMusic();
-//		music.music();
-		StartMenu start = new StartMenu(WIDTH,HEIGHT,"EXTREMELY DIFFICULT QUIZ");
-		//window = new Window(WIDTH, HEIGHT, "Extremely Difficult Quiz");
+		menu = new Menu(this);
+		
+		this.addMouseListener(new MouseInput(menu, this));
+		handler=new Handler(this);
+		setPreferredSize(new Dimension(WIDTH,HEIGHT));
+		setMaximumSize(new Dimension(WIDTH,HEIGHT));
+		setMinimumSize(new Dimension(WIDTH,HEIGHT));
+		
+		q1 = new Q1();
+		correct = new Correct();
+		
+		JFrame frame = new JFrame(TITLE);
+		frame.add(this);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	
 		start();
 	}
 	
-	public void start(){
+	
+	public enum STATE{
+		MENU,
+		GAME,
+		Q1,
+		CORRECT
+	};
+	
+	public static STATE state = STATE.MENU;
+
+	public void init(){
+	
+		
+	
+	}
+	private synchronized void start(){
+		if(running)
+			return;
+
 		running = true;
-		run();
+		thread = new Thread(this);
+		thread.start();
 	}
-	
-	public void stop(){
+
+	private synchronized void stop(){
+		if(!running)
+			return;
+
 		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.exit(1);
 	}
-	
+
 	public void run(){
+		init();
 		long lastTime = System.nanoTime();
-		double amountOfTicks = 60.0;
+		final double amountOfTicks = 60.0;
 		double ns = 1000000000 / amountOfTicks;
 		double delta = 0;
-		long timer = System.currentTimeMillis();
+		int updates = 0;
 		int frames = 0;
+		long timer = System.currentTimeMillis();
+
 		while (running){
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while (delta >= 1){
+			if(delta >= 1){
 				tick();
+				updates++;
 				delta--;
 			}
-			if (running){
-				render();
-			}
+			render();
 			frames++;
-			
-			//Display the FPS in the console
-			if (System.currentTimeMillis() - timer > 1000){
+
+			if(System.currentTimeMillis() - timer > 1000){
 				timer += 1000;
-				System.out.println("FPS: " + frames);
+				System.out.println(updates + " Ticks, Fps " + frames);
+				updates = 0;
 				frames = 0;
 			}
 		}
 		stop();
 	}
-	
-	public void tick(){
+
+	private void tick(){
+		if(state == STATE.MENU){
+			menu.tick();
+		}
 		
 	}
 	
-	public void render(){
-		
+	public STATE getState(){
+		return STATE.MENU;
 	}
-	
+
+	private void render(){
+
+		BufferStrategy bs = this.getBufferStrategy();
+
+		if(bs == null){
+			createBufferStrategy(3);
+			return;
+		}
+
+		Graphics g = bs.getDrawGraphics();
+		if(state==STATE.MENU){
+		/////////////////////////////
+//			g.setColor(Color.black);
+//			g.fillRect(0, 0, 1200, 900);
+		menu.render(g);
+			
+			
+		/////////////////////////////
+		}else if (state == STATE.Q1){
+			q1.render(g);
+		}else if (state == STATE.CORRECT){
+			correct.render(g);
+		}
+		g.dispose();
+		bs.show();
+	}
+
 	public static void main(String[] args){
 		Game game = new Game();
+		game.start();
+		
 	}
 }
