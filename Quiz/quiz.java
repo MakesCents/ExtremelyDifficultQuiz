94e26e2a9fcefafb34f4f57c91c42b5a4d8a6270
MakesCents <jaredfunk28@gmail.com>
2016-03-23 12:06:49 -0500

    Getting Applets to work by ignoring a lot of security warnings

--- Quiz/quiz.java
+++ Quiz/quiz.java
@@ -1,8 +1,76 @@
-import java.applet.Applet;
-import java.awt.Graphics;
+// TestApplet.java
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.applet.*;
+import javax.swing.*;
+import javax.swing.border.*;
+public class quiz extends JApplet implements ActionListener {
+  private JPanel pane = null;
+  private JScrollPane scrolling = null;
+  private JTextPane fileBox = null;
+  private JTextField tfFilename = null;
+  private JButton butLoad = null;
+  private final String LOAD = "load";
+  public void init() {
+      try {
+            jbInit();
+      } catch(Exception e) {
+        e.printStackTrace();
+      }
+  }
+  
+   // method which will read data from file, and return it in
+      // String
+  public String readFile(String fn) {
+     String thisLine, ret = "";
+     try {
+       FileInputStream fin =  new FileInputStream(fn);
+       BufferedReader myInput = new BufferedReader
+                         (new InputStreamReader(fin));
+       while ((thisLine = myInput.readLine()) != null) {  
+         ret += thisLine + "\n";
+       }
+     } catch (Exception e) {
+       ret = "Cannot load, exception!";
+     }
+     return ret;
+   }
 
-public class quiz extends Applet {
-    public void paint(Graphics g) {
-        g.drawString("Hello world!", 50, 25);
-    }
-}
+   private void jbInit() throws Exception {
+     pane = new JPanel();
+     pane.setBounds(new Rectangle(0, 0, 500, 325));
+     pane.setLayout(null);
+     pane.setBorder(BorderFactory.createEtchedBorder(
+                       EtchedBorder.LOWERED));
+     pane.setBackground(new Color(221, 194, 219));
+
+     fileBox = new JTextPane();
+     fileBox.setText("");
+     fileBox.setEditable(false);
+     scrolling = new JScrollPane(fileBox);
+     scrolling.setBounds(new Rectangle(16, 65, 295, 225));
+
+     tfFilename = new JTextField();
+     tfFilename.setText("");
+     tfFilename.setBounds(new Rectangle(16, 23, 206, 29));
+
+     butLoad = new JButton();
+     butLoad.setBounds(new Rectangle(231, 23, 80, 30));
+     butLoad.setText("Load");
+     butLoad.setActionCommand(LOAD);
+     butLoad.addActionListener(this);
+
+     pane.add(scrolling);
+     pane.add(tfFilename);
+     pane.add(butLoad);
+
+     setContentPane(pane);
+   }
+
+   public void actionPerformed(ActionEvent e) {
+     if (e.getActionCommand().equals(LOAD)) {
+         fileBox.setText(readFile(tfFilename.getText()));
+     }
+   }
+ }
