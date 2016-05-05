b85405910217a035a74765f17305d9b9bfa13a9f
Jared Funk <jaredfunk28@gmail.com>
2016-05-05 13:32:38 -0500

    We'll add them again later

--- ExtremelyDifficultQuiz/src/UnitTesting.java
+++ ExtremelyDifficultQuiz/src/UnitTesting.java
@@ -27,37 +27,5 @@ public class UnitTesting extends TestCase {
 	
 	
 	
-	public void setUp() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-		game = new Game();
-		menu = new Menu(game);
-		
-		correct = new Correct(game, q2, q3);
-//		game.setVisible(true);
-		timer = new Timer(game);
-		mi = new MouseInput(menu, game, correct, q1, timer);
-		meMenu = new MouseEvent(game, 0, 0, 0, 612, 450, 0, false);
-		meQ1 = new MouseEvent(game, 0, 0, 0, 600, 600, 0, false);
-		meQ3 = new MouseEvent(game, 0, 0, 0, 600, 450, 0, false);
-		meQ4 = new MouseEvent(game, 0, 0, 0, 600, 300, 0, false);
-	}
 
-	
-	public void testcorrectScreen(){
-		mi.mouseReleased(meMenu);
-		if(game.getState() == (Game.STATE.Q1)|| game.getState() == Game.STATE.Q2){
-			mi.mouseReleased(meQ1);
-		}else if(game.getState() == Game.STATE.Q3){
-			mi.mouseReleased(meQ3);
-		}else if(game.getState()== Game.STATE.Q4 || game.getState() == Game.STATE.Q5){
-			mi.mouseReleased(meQ4);
-		}
-		assertTrue(game.getState() == Game.STATE.CORRECT);
-	}
-	public void testLoseScreen() throws InterruptedException{
-		Thread.sleep(5000);
-		assertTrue(game.getState() == STATE.GAME.LOSE);
-	}
-	public void testSoundClick(){
-		
-	}
 }
