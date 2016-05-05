a41f3e59bff85026d31d8639cd61db9ef6b97cb5
Jared Funk <jaredfunk28@gmail.com>
2016-05-04 20:22:37 -0500

    Q10 Fixes

--- ExtremelyDifficultQuiz/src/Q10.java
+++ ExtremelyDifficultQuiz/src/Q10.java
@@ -20,26 +20,25 @@ public class Q10 extends Question{
 
 	public void render(Graphics g){
 		g.setColor(Color.black);
-		g.fillRect(0, 0, Game.WIDTH + 20, Game.HEIGHT + 20);
+		g.fillRect(0, 0, (int)(Game.widthRatio *Game.WIDTH) + 20, (int)(Game.heightRatio *Game.HEIGHT) + 20);
 		g.setColor(Color.white);
-		g.fillOval(10, 10, 50, 50);
+		g.fillOval((int)(Game.widthRatio*10), (int)(Game.heightRatio *10), (int)(Game.widthRatio *50), (int)(Game.heightRatio *50));
 		g.setColor(Color.blue);
-		Font fnt = new Font("arial black", 1, 50);
+		Font fnt = new Font("arial black", 1, (int)(40*Math.min(Game.widthRatio, Game.heightRatio)));
 		g.setFont(fnt);
-		//g.drawString("1", 20, 50);
-		g.fillRect(100, 100, 1000, 100);
+		g.fillRect((int)(Game.widthRatio*100), (int)(Game.heightRatio*100), (int)(Game.widthRatio*1000), (int)(Game.heightRatio*100));
 		g.setColor(Color.white);
-		g.drawString("What is Mario's Original Name", 150, 150);
+		g.drawString("What is Mario's Original Name", (int)(Game.widthRatio*150), (int)(Game.heightRatio*150));
 		g.setColor(Color.blue);
-		g.fillRect(100, 250, 800, 100);
-		g.fillRect(100, 400, 800, 100);
-		g.fillRect(100, 550, 800, 100);
-		g.fillRect(100, 700, 800, 100);
+		g.fillRect((int)(Game.widthRatio*100), (int)(Game.heightRatio*250), (int)(Game.widthRatio*800), (int)(Game.heightRatio*100));
+		g.fillRect((int)(Game.widthRatio*100), (int)(Game.heightRatio*400), (int)(Game.widthRatio*800), (int)(Game.heightRatio*100));
+		g.fillRect((int)(Game.widthRatio*100), (int)(Game.heightRatio*550), (int)(Game.widthRatio*800), (int)(Game.heightRatio*100));
+		g.fillRect((int)(Game.widthRatio*100), (int)(Game.heightRatio*700), (int)(Game.widthRatio*800), (int)(Game.heightRatio*100));
 		g.setColor(Color.white);
-		g.drawString("Luigi", 120, 290);
-		g.drawString("Drake's Song", 120, 440);
-		g.drawString("Stephen Hawking's Middle Name", 120, 590);
-		g.drawString("Chi Wong Feng", 120, 740);
+		g.drawString("Luigi", (int)(Game.widthRatio*120), (int)(Game.heightRatio*290));
+		g.drawString("Jumpman", (int)(Game.widthRatio*120), (int)(Game.heightRatio*440));
+		g.drawString("Stephen Hawking's Middle Name", (int)(Game.widthRatio*120), (int)(Game.heightRatio*590));
+		g.drawString("404 Page not found", (int)(Game.widthRatio*120), (int)(Game.heightRatio*740));
 		timer.render(g);
 	}
 
@@ -55,6 +54,6 @@ public class Q10 extends Question{
 	}
 	
 	public Game.STATE getState(){
-		return Game.STATE.Q5;
+		return Game.STATE.Q10;
 	}
 }
